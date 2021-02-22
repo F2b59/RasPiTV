f6a0293a493347137f0977d180d3d83d0e0cb6c9
F2b59 <34244826+F2b59@users.noreply.github.com>
2021-02-22 22:13:35 +0100

    test

--- channels-old/01.sh
+++ channels-old/01.sh
@@ -1,2 +1,2 @@
 killall omxplayer.bin
-nohup omxplayer rtp://233.32.36.161:7500/3161 > nohup.out 2> nohup.err < /dev/null &
+nohup omxplayer --live --with-info --stats rtp://233.32.36.161:7500/3161 > nohup.out 2> nohup.err < /dev/null &
