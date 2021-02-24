156c5aa62963cbd0e25805696b54030dde8cbc65
F2b59 <34244826+F2b59@users.noreply.github.com>
2021-02-24 12:06:14 +0100

    add --live

--- channels/07.sh
+++ channels/07.sh
@@ -1,2 +1,2 @@
 killall omxplayer.bin
-nohup omxplayer -o alsa:hw:CARD=Device rtp://233.136.44.27:7500/4106 > nohup.out 2> nohup.err < /dev/null &
+nohup omxplayer --live -o alsa:hw:CARD=Device rtp://233.136.44.27:7500/4106 > nohup.out 2> nohup.err < /dev/null &
