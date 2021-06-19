98ef5458fdf459e28c7bb693a00bc835632d1244
F2b59 <34244826+F2b59@users.noreply.github.com>
2021-06-19 12:23:59 +0200

    cleaner /list code

--- server.py
+++ server.py
@@ -128,7 +128,7 @@ def stop():
 
 @app.route('/list')
 def displayList():
-    return '<html><body>' + '<br>'.join([str(i+1) + ' - ' + channels.List[i][1] + ' - ' + channels.List[i][0] for i in range(len(channels.List))]) + '</body></html>'
+    return '<html>\n<body>\n\n' + '<br>\n'.join([str(i+1) + ' - ' + channels.List[i][1] + ' - ' + channels.List[i][0] for i in range(len(channels.List))]) + '\n\n</body>\n</html>'
 
 
 if __name__ == '__main__':
